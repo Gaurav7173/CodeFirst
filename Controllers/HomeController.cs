918210d6e8d68d87e5a8a8a357fa87322bd466cc
Gaurav7173 <gaurav.kumar7173.com>
2025-09-16 05:39:20 +0530

    Update and Delete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,13 +28,15 @@ public class HomeController : Controller
     }
 
     [HttpPost]
+    [ValidateAntiForgeryToken]
     public async Task<IActionResult> Create(Student std)
     {
-        if(ModelState.IsValid)
+        if (ModelState.IsValid)
         {
             await studentDB.Students.AddAsync(std); //Adding the new student entity to the Students DbSet
             await studentDB.SaveChangesAsync(); //Saving changes to the database
-            return RedirectToAction("Index","Home"); //Redirecting to the Index action after successful creation
+            TempData["Insert_Success"] = "Student record inserted successfully"; //Setting a success message in TempData to be displayed after redirection
+            return RedirectToAction("Index", "Home"); //Redirecting to the Index action after successful creation
         }
         return View(std); //If the model state is not valid, return the view with the current student data to display validation errors
     }
@@ -54,6 +56,75 @@ public class HomeController : Controller
         return View(stdData); //Passing the list of students to the view for rendering
         return View();
     }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null || studentDB.Students == null)
+        {
+            return NotFound();
+        }
+        var stdData=await studentDB.Students.FindAsync(id); //Retrieving all student records from the Students DbSet and converting them to a list
+
+        if (stdData == null)
+        {
+            return NotFound();
+        }
+        return View(stdData);   //Passing the list of students to the view for rendering
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int? id, Student std)
+    {
+        if (id != std.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            studentDB.Update(std); //Adding the new student entity to the Students DbSet
+            await studentDB.SaveChangesAsync();
+            TempData["update_Success"] = "Student record updated successfully";
+            return RedirectToAction("Index", "Home"); //Redirecting to the Index action after successful creation
+        }
+
+        return View(std);
+    }
+
+      public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null || studentDB.Students == null)
+        {
+            return NotFound();
+        }
+
+        var stdData=await studentDB.Students.FirstOrDefaultAsync(x=>x.Id==id); //Retrieving all student records from the Students DbSet and converting them to a list
+
+        if (stdData == null)
+        {
+            return NotFound();
+        }
+        return View(stdData);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int? id)
+    {
+        var stdData = await studentDB.Students.FindAsync(id);
+        if (stdData != null)
+        {
+            studentDB.Students.Remove(stdData); //removing the new student entity to the Students DbSet
+
+        }
+        await studentDB.SaveChangesAsync(); //Saving changes to the database
+        TempData["delete_Success"] = "Student record deleted successfully";
+        return RedirectToAction("Index", "Home");
+
+
+    }
+    
     
     public IActionResult Privacy()
     {
